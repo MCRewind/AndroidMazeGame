46307f0536fe9a50f85e194f06d4f18f8a48fffa
Clayton Meyer <sploing1@gmail.com>
2017-03-01 13:37:10 -0800

    added basic shape drawing

--- app/src/main/java/com/ghs/mazegame/MyGLRenderer.java
+++ app/src/main/java/com/ghs/mazegame/MyGLRenderer.java
@@ -1,9 +1,9 @@
 package com.ghs.mazegame;
 
-import android.opengl.EGLConfig;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 
+import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 /**
@@ -11,19 +11,39 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
-    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+    private Triangle mTriangle;
+    private Square   mSquare;
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+
+        // initialize a triangle
+        mTriangle = new Triangle();
+        // initialize a square
+        mSquare = new Square();
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-    }
 
-    @Override
-    public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
-
+        mTriangle.draw();
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
