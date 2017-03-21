2860bdaa989ec246fd5da467534764752df0ecae
Joshua Wentzel <joshuahwentzel@gmail.com>
2017-03-21 13:30:01 -0700

    Made surfaceView into a seperate class so that I could call onTouched() to detect touches.

--- app/src/main/java/com/ghs/mazegame/MainActivity.java
+++ app/src/main/java/com/ghs/mazegame/MainActivity.java
@@ -6,25 +6,32 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    private GLSurfaceView surface;
+    //private GLSurfaceView surface;
+    private MyGLSurfaceView mySurfaceView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
 
         surface = new GLSurfaceView(this);
         surface.setEGLContextClientVersion(2);
         surface.setRenderer(new Renderer());
-
         setContentView(surface);
+        */
+        mySurfaceView = new MyGLSurfaceView(this);
+        mySurfaceView.setEGLContextClientVersion(2);
+        mySurfaceView.setRenderer(new Renderer());
+        mySurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        setContentView(mySurfaceView);
     }
 
     public void onPause() {
         super.onPause();
-        surface.onPause();
+        mySurfaceView.onPause();
     }
 
     public void onResume() {
         super.onResume();
-        surface.onResume();
+        mySurfaceView.onResume();
     }
 }
