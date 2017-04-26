baa7ce0ca4499fd6fd0d4ac043c2f91241d12574
Clayton Meyer <sploing1@gmail.com>
2017-04-26 12:24:51 -0700

    Disabled navigation bar

--- app/src/main/java/com/ghs/mazegame/game/MainActivity.java
+++ app/src/main/java/com/ghs/mazegame/game/MainActivity.java
@@ -2,6 +2,7 @@ package com.ghs.mazegame.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ghs.mazegame.engine.display.Surface;
 
@@ -17,6 +18,14 @@ public class MainActivity extends Activity {
         surface.setRenderer(new Renderer(surface.getResources()));
 
         setContentView(surface);
+        
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     public void onPause() {
