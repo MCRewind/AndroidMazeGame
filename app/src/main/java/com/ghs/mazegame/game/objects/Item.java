0ff131d0fe0705ba9d35fd40e71a26e12a5bdccd
Clayton Meyer <sploing1@gmail.com>
2017-05-18 13:39:13 -0700

    added key previews and added items to tile bar

--- app/src/main/java/com/ghs/mazegame/game/objects/Item.java
+++ app/src/main/java/com/ghs/mazegame/game/objects/Item.java
@@ -1,20 +1,148 @@
 package com.ghs.mazegame.game.objects;
 
+import android.renderscript.Matrix4f;
+
+import com.ghs.mazegame.engine.components.Shader;
+import com.ghs.mazegame.engine.components.Texture;
+import com.ghs.mazegame.engine.components.VAO;
+import com.ghs.mazegame.engine.display.Camera;
 import com.ghs.mazegame.game.interfaces.GameObject;
 
-/**
- * Created by cmeyer3887 on 5/18/2017.
- */
+import static com.ghs.mazegame.game.Renderer.defaultShader;
 
 public class Item implements GameObject {
 
-    @Override
-    public void update() {
+    private float x = 0, y = 0;
+    private float width = 0, height = 0;
+    private Camera camera;
+    private VAO vao;
+    private Texture texture;
+    private Shader shader;
+    private String name;
 
+    public Item(String name, Camera camera, Texture texture, Shader shader, float x, float y, float width, float height, float depth) {
+        this.name = name;
+        this.texture = texture;
+        this.shader = shader;
+        this.camera = camera;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        float[] vertices = new float[] {
+                0.0f,  0.0f,   depth, //TOP LEFT
+                0.0f,  height, depth, //BOTTOM LEFT
+                width, height, depth, //BOTTOM RIGHT
+                width, 0.0f,   depth  //TOP RIGHT
+        };
+        int[] indices = new int[] {
+                0, 1, 3,
+                1, 2, 3
+        };
+        float[] texCoords = new float[] {
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0
+        };
+        vao = new VAO(vertices, indices, texCoords);
     }
 
-    @Override
-    public void render() {
+    public Item(Camera camera, Texture texture, float x, float y, float width, float height, float depth) {
+        this.texture = texture;
+        shader = defaultShader;
+        this.camera = camera;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        float[] vertices = new float[] {
+                0.0f,  0.0f,   depth, //TOP LEFT
+                0.0f,  height, depth, //BOTTOM LEFT
+                width, height, depth, //BOTTOM RIGHT
+                width, 0.0f,   depth  //TOP RIGHT
+        };
+        int[] indices = new int[] {
+                0, 1, 3,
+                1, 2, 3
+        };
+        float[] texCoords = new float[] {
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0
+        };
+        vao = new VAO(vertices, indices, texCoords);
+    }
 
+    public void update() {}
+
+    public void render() {
+        Matrix4f proj = camera.getUnatransformedProjection();
+        proj.translate(x, y, 0);
+        shader.setUniformMat4f("projection", proj);
+        texture.bind();
+        shader.enable();
+        vao.render();
+        shader.disable();
+        texture.unbind();
+    }
+
+    public boolean contains(float x, float y) {
+        if ((x >= this.x && x < this.x + this.width) && (y >= this.y && y < this.y + this.height)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    public void translate(float dx, float dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
     }
 }
