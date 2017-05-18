7d36df62a891e80c8ad7dc71ca8fdab920044cff
Clayton Meyer <sploing1@gmail.com>
2017-05-18 13:18:01 -0700

    worked on item

---
+++ app/src/main/java/com/ghs/mazegame/game/objects/Item.java
@@ -0,0 +1,20 @@
+package com.ghs.mazegame.game.objects;
+
+import com.ghs.mazegame.game.interfaces.GameObject;
+
+/**
+ * Created by cmeyer3887 on 5/18/2017.
+ */
+
+public class Item implements GameObject {
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
